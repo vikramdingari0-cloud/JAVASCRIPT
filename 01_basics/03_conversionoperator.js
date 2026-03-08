99076890144ac698fd09c87cba1a9484314e7d6d
vikramdingari0-cloud <vikramdingari0@gmail.com>
2026-03-08 20:19:10 +0530

    update files

--- 01_basics/03_conversionoperator.js
+++ 01_basics/03_conversionoperator.js
@@ -4,10 +4,39 @@ let x = Number(score)
 console.log(typeof x);
  let loggegin = "vikram"
  let booleanin = Boolean(loggegin)
- console.log(booleanin);
- console.log(typeof booleanin);
+//  console.log(booleanin);
+//  console.log(typeof booleanin);
 
  //1->true  ,  0->false
  //""->false   ,   "vikram"->true
  
+//console.log("vikram");
  
+// console.log(2+2);  ->4
+// console.log(2-2);  ->0
+// console.log(2*2);  ->4
+// console.log(2/2);  ->1
+// console.log(2%2);  ->0
+// console.log(2**2); ->4
+
+// let str1 = "dingari"
+// let str2 = "vikram"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + 2);  ->3
+// console.log("1" + 2);  ->12
+// console.log(1 + "2");  ->12
+// console.log("1" + 2 + 2);  ->122
+// console.log(1 + 2 + "2");  ->32
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
