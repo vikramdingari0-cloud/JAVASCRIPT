00000069322e81954fe9f3a1344447952335c2b2
vikramdingari0-cloud <vikramdingari0@gmail.com>
2026-03-08 20:01:02 +0530

    update files

---
+++ 01_basics/03_conversionoperator.js
@@ -0,0 +1,13 @@
+let score = "33"
+console.log(typeof score);
+let x = Number(score)
+console.log(typeof x);
+ let loggegin = "vikram"
+ let booleanin = Boolean(loggegin)
+ console.log(booleanin);
+ console.log(typeof booleanin);
+
+ //1->true  ,  0->false
+ //""->false   ,   "vikram"->true
+ 
+ 
